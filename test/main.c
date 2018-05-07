22647526427154adabb184fd8dde17a149c3daf1
Daria <dariapancenko717@gmail.com>
2018-05-08 02:21:44 +0700

    Knight test

--- test/main.c
+++ test/main.c
@@ -29,17 +29,28 @@ CTEST(P_Move, Incor) {
 }
 
 CTEST(Rook_Move, Cor) {
-    int result = board_func("d1-d5", 1);
+    int result = board_func("a1-a5", 1);
     int expected = 0;
     ASSERT_EQUAL(expected,result);
 }
 
 CTEST(Rook_Move, Incor) {
-    int result = board_func("b1-b3", 1);
+    int result = board_func("a1-c4", 1);
     int expected = -1;
     ASSERT_EQUAL(expected,result);
 }
 
+CTEST(Knight_Move, Cor) {
+    int result = board_func("b1-c3", 1);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(Knight_Move, Incor) {
+    int result = board_func("b1-b3", 1);
+    int expected = -1;
+    ASSERT_EQUAL(expected,result);
+}
 
 int main(int argc, const char** argv)
 {   _board();
